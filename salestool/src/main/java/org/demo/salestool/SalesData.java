6d40030aaed845fbfe7a1b8a5d2778db290a86f3
RAJAMADHURI\RajaMadhu <rsekhar3602@gmail.com>
2017-04-07 12:19:39 -0400

    First Change

--- salestool/src/main/java/org/demo/salestool/SalesData.java
+++ salestool/src/main/java/org/demo/salestool/SalesData.java
@@ -4,6 +4,8 @@ public class SalesData {
 	
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "We are in Sales Data Class !!!" );
+	        System.out.println( "We are in Sales Data Class  !!!" );
+	        
+	        System.out.println( "Whats up Vrindha" );
 	    }
 }
