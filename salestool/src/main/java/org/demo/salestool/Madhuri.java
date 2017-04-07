806b81d17c69da150f6cab870a76aedc8768c7ec
RAJAMADHURI\RajaMadhu <rsekhar3602@gmail.com>
2017-04-07 16:07:19 -0400

    Adding Madhuri Java Class

---
+++ salestool/src/main/java/org/demo/salestool/Madhuri.java
@@ -0,0 +1,11 @@
+package org.demo.salestool;
+
+public class Madhuri {
+	
+	 public static void main( String[] args )
+	    {
+	        
+	        
+	        System.out.println( "Feeding Vrindha" );
+	    }
+}
