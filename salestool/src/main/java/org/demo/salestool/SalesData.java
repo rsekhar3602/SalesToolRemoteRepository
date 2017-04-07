45306f16385bc7e3771dad613ffb4b1769d9bcbf
RAJAMADHURI\RajaMadhu <rsekhar3602@gmail.com>
2017-04-07 12:05:11 -0400

    Initial Commit to Local Sales Reository.

---
+++ salestool/src/main/java/org/demo/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package org.demo.salestool;
+
+public class SalesData {
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "We are in Sales Data Class !!!" );
+	    }
+}
