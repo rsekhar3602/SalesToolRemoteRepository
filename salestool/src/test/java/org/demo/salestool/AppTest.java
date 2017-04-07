62cc6d1c29bbb2ab84ffb5e70aeec4291a9e0131
RAJAMADHURI\RajaMadhu <rsekhar3602@gmail.com>
2017-04-07 14:41:00 -0400

    App Test Java File update

--- salestool/src/test/java/org/demo/salestool/AppTest.java
+++ salestool/src/test/java/org/demo/salestool/AppTest.java
@@ -1,38 +1,28 @@
 package org.demo.salestool;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void simpleTestPass() {
+               int a = 1;
+               int b = 2;
+               assertTrue(a + b == 3);
+		
+	}
+	
+	@Test
+	public void simpleTestFail() {
+               int a = 2;
+               int b = 2;
+               assertTrue(a + b == 4);
+		
+	}
 }
